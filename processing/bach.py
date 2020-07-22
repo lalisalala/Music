3452c09bddedc2803e851c3344d21b4787d2123d
Lisa <ga27bil@mytum.de>
2020-07-22 14:35:40 +0200

    processes folder

---
+++ processing/bach.py
@@ -0,0 +1,119 @@
+import os
+import time
+import magenta
+from magenta.models.polyphony_rnn import polyphony_model
+from magenta.models.polyphony_rnn import polyphony_sequence_generator
+from magenta.models.shared import sequence_generator_bundle
+from magenta.music import constants
+from magenta.music.protobuf import generator_pb2
+from magenta.music.protobuf import music_pb2
+import tensorflow.compat.v1 as tf
+from config import cfg
+
+
+def get_bundle():
+    """Returns a generator_pb2.GeneratorBundle object based read from bundle_file.
+
+    Returns:
+      Either a generator_pb2.GeneratorBundle or None if the bundle_file flag is
+      not set or the save_generator_bundle flag is set.
+    """
+    bundle_file = os.path.expanduser(cfg['bundle_file_bach'])
+    return sequence_generator_bundle.read_bundle_file(bundle_file)
+
+
+def run_with_flags(generator, midi_path=None):
+    """Generates polyphonic tracks and saves them as MIDI files.
+
+    Uses the options specified by the flags defined in this module.
+
+    Args:
+      generator: The PolyphonyRnnSequenceGenerator to use for generation.
+    """
+    output_dir = os.path.expanduser(cfg['output_dir'])
+    primer_midi = os.path.expanduser(midi_path)
+
+    if not tf.gfile.Exists(output_dir):
+        tf.gfile.MakeDirs(output_dir)
+
+    qpm = magenta.music.DEFAULT_QUARTERS_PER_MINUTE
+    primer_sequence = magenta.music.midi_file_to_sequence_proto(primer_midi)
+    if primer_sequence.tempos and primer_sequence.tempos[0].qpm:
+        qpm = primer_sequence.tempos[0].qpm
+    else:
+        tf.logging.warning(
+            'No priming sequence specified. Defaulting to empty sequence.')
+        primer_sequence = music_pb2.NoteSequence()
+        primer_sequence.tempos.add().qpm = qpm
+        primer_sequence.ticks_per_quarter = constants.STANDARD_PPQ
+
+    # Derive the total number of seconds to generate.
+    seconds_per_step = 60.0 / qpm / generator.steps_per_quarter
+    generate_end_time = cfg['num_steps'] * seconds_per_step + 2
+
+    # Specify start/stop time for generation based on starting generation at the
+    # end of the priming sequence and continuing until the sequence is num_steps
+    # long.
+    generator_options = generator_pb2.GeneratorOptions()
+    # Set the start time to begin when the last note ends.
+    generate_section = generator_options.generate_sections.add(
+        start_time=primer_sequence.total_time,
+        end_time=generate_end_time)
+
+    if generate_section.start_time >= generate_section.end_time:
+        tf.logging.fatal(
+            'Priming sequence is longer than the total number of steps '
+            'requested: Priming sequence length: %s, Total length '
+            'requested: %s',
+            generate_section.start_time, generate_end_time)
+        return
+
+    generator_options.args['temperature'].float_value = 1.0
+    generator_options.args['beam_size'].int_value = 1
+    generator_options.args['branch_factor'].int_value = 1
+    generator_options.args[
+        'steps_per_iteration'].int_value = 1
+    generator_options.args['condition_on_primer'].bool_value = (
+        True)
+    generator_options.args['no_inject_primer_during_generation'].bool_value = (
+        not False)
+
+    tf.logging.debug('primer_sequence: %s', primer_sequence)
+    tf.logging.debug('generator_options: %s', generator_options)
+
+    # Make the generate request num_outputs times and save the output as midi
+    # files.
+    date_and_time = time.strftime('%Y-%m-%d_%H%M%S')
+    digits = len(str(1))
+    for i in range(1):
+        generated_sequence = generator.generate(primer_sequence, generator_options)
+
+        midi_filename = primer_midi.replace('midi', 'bach.midi')
+        midi_path = os.path.join(output_dir, midi_filename)
+        magenta.music.sequence_proto_to_midi_file(generated_sequence, midi_path)
+
+    tf.logging.info('Wrote %d MIDI files to %s',
+                    1, output_dir)
+
+
+def main(unused_argv):
+    """Saves bundle or runs generator based on flags."""
+    tf.logging.set_verbosity('INFO')
+
+    bundle = get_bundle()
+
+    config_id = bundle.generator_details.id
+    config = polyphony_model.default_configs[config_id]
+    config.hparams.parse(cfg['hparams'])
+    # Having too large of a batch size will slow generation down unnecessarily.
+    config.hparams.batch_size = min(
+        config.hparams.batch_size, 1)
+
+    generator = polyphony_sequence_generator.PolyphonyRnnSequenceGenerator(
+        model=polyphony_model.PolyphonyRnnModel(config),
+        details=config.details,
+        steps_per_quarter=config.steps_per_quarter,
+        checkpoint=None,
+        bundle=bundle)
+
+    run_with_flags(generator, unused_argv)
---
+++ processing/transcription.py
@@ -0,0 +1,105 @@
+"""Transcribe a recording of piano audio."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+from magenta.models.onsets_frames_transcription import audio_label_data_utils
+from magenta.models.onsets_frames_transcription import infer_util
+from magenta.models.onsets_frames_transcription import train_util
+from magenta.music import midi_io
+from magenta.music.protobuf import music_pb2
+import six
+import tensorflow.compat.v1 as tf
+from config import cfg
+
+
+def create_example(filename, sample_rate, load_audio_with_librosa):
+    """Processes an audio file into an Example proto."""
+    wav_data = tf.gfile.Open(filename, 'rb').read()
+    example_list = list(
+        audio_label_data_utils.process_record(
+            wav_data=wav_data,
+            sample_rate=sample_rate,
+            ns=music_pb2.NoteSequence(),
+            # decode to handle filenames with extended characters.
+            example_id=six.ensure_text(filename, 'utf-8'),
+            min_length=0,
+            max_length=-1,
+            allow_empty_notesequence=True,
+            load_audio_with_librosa=load_audio_with_librosa))
+    assert len(example_list) == 1
+    return example_list[0].SerializeToString()
+
+
+def run(argv, config_map, data_fn):
+    """Create transcriptions."""
+    tf.logging.set_verbosity('INFO')
+
+    config = config_map[cfg['config_transcription']]
+    hparams = config.hparams
+    hparams.parse(cfg['hparams'])
+    hparams.batch_size = 1
+    hparams.truncated_length_secs = 0
+
+    with tf.Graph().as_default():
+        examples = tf.placeholder(tf.string, [None])
+
+        dataset = data_fn(
+            examples=examples,
+            preprocess_examples=True,
+            params=hparams,
+            is_training=False,
+            shuffle_examples=False,
+            skip_n_initial_records=0)
+
+        estimator = train_util.create_estimator(config.model_fn,
+                                                os.path.expanduser(cfg['model_dir']),
+                                                hparams)
+
+        iterator = dataset.make_initializable_iterator()
+        next_record = iterator.get_next()
+
+        with tf.Session() as sess:
+            sess.run([
+                tf.initializers.global_variables(),
+                tf.initializers.local_variables()
+            ])
+
+            for filename in argv[1:]:
+                tf.logging.info('Starting transcription for %s...', filename)
+
+                # The reason we bounce between two Dataset objects is so we can use
+                # the data processing functionality in data.py without having to
+                # construct all the Example protos in memory ahead of time or create
+                # a temporary tfrecord file.
+                tf.logging.info('Processing file...')
+                sess.run(iterator.initializer,
+                         {examples: [
+                             create_example(filename, hparams.sample_rate,
+                                            0)]})
+
+                def transcription_data(params):
+                    del params
+                    return tf.data.Dataset.from_tensors(sess.run(next_record))
+
+                input_fn = infer_util.labels_to_features_wrapper(transcription_data)
+
+                tf.logging.info('Running inference...')
+                checkpoint_path = None
+                prediction_list = list(
+                    estimator.predict(
+                        input_fn,
+                        checkpoint_path=checkpoint_path,
+                        yield_single_examples=False))
+                assert len(prediction_list) == 1
+
+                sequence_prediction = music_pb2.NoteSequence.FromString(
+                    prediction_list[0]['sequence_predictions'][0])
+
+                midi_filename = filename + '.midi'
+                midi_io.sequence_proto_to_midi_file(sequence_prediction, midi_filename)
+
+                tf.logging.info('Transcription written to %s.', midi_filename)
